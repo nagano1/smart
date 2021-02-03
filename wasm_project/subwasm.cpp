ef67394f36ce3a1c50abcd2089f0f53a50a000e1
nanashi <nanashi@dummy.com>
2021-02-03 09:35:24 +0900

    allOk wasm

--- wasm_project/subwasm.cpp
+++ wasm_project/subwasm.cpp
@@ -18,7 +18,7 @@ extern "C" void* memory;
 extern "C" {
 
     int calling2(int p) {
-        print("{\"isTestResult\":true, \"ok\": true}");
+        print("{\"isTestResult\":true, \"ok\": false}");
         return 330 + p;
     }
 
