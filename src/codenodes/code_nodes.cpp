ac49259f7c5f7589352b031b71fc02477fb3dde2
nanashi <nanashi@dummy.com>
2021-02-06 06:06:14 +0900

    do the trick2

--- src/codenodes/code_nodes.cpp
+++ src/codenodes/code_nodes.cpp
@@ -109,6 +109,9 @@ namespace smart {
 
         memset(this->entries, 0, sizeof(this->entries));
         for (int i = 0; i < HashNode_TABLE_SIZE; i++) {
+            this->entries[i] = nullptr;
+        }
+        for (int i = 0; i < HashNode_TABLE_SIZE; i++) {
             if (this->entries[i] != nullptr) {
                 throw 3;
             }
