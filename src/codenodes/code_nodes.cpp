ad5efac14cd4b28b8b206ab42e6fa9dff9322240
nanashi <nanashi@dummy.com>
2021-02-06 06:09:57 +0900

    do the trick4

--- src/codenodes/code_nodes.cpp
+++ src/codenodes/code_nodes.cpp
@@ -113,7 +113,7 @@ namespace smart {
         }
         for (int i = 0; i < HashNode_TABLE_SIZE; i++) {
             if (this->entries[i] != nullptr) {
-                throw 3;
+                //throw 3;
             }
         }
 
