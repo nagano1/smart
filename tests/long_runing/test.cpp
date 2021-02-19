e1820632146b59108a0d7887f55c4ebd4178c4c9
nanashi <nanashi@dummy.com>
2021-02-20 06:23:05 +0900

    long running2

---
+++ tests/long_runing/test.cpp
@@ -0,0 +1,27 @@
+﻿#include "test.h"
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <mutex>
+#include <thread>
+#include <array>
+#include <algorithm>
+
+
+#include <cstdlib>
+#include <cassert>
+#include <cstdio>
+#include <chrono>
+#include <atomic>
+#include <unordered_map>
+#include <vector>
+
+#include <cstdint>
+#include <ctime>
+#include <emmintrin.h>
+
+int main(int argc, char **argv) {
+    printf("G");
+	return 0;
+}
