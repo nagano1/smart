ae5ae8cfe355f118b85bc4497efe220ffa7e5884
nanashi <nanashi@dummy.com>
2021-02-21 07:25:10 +0900

    heavey load 2

--- tests/long_running/long_running.cpp
+++ tests/long_running/long_running.cpp
@@ -60,7 +60,7 @@ int main(int argc, char **argv)
         free(charBuffer3);
     }
 */
-for (int i = 0; i < 200*1000; i++) 
+for (int i = 0; i < 10*1000*1000; i++) 
     {
         //std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
