6386f0d5718b6c8da8875acafd586fded840ef88
nanashi <nanashi@dummy.com>
2021-02-21 07:20:13 +0900

    heavey load

--- tests/long_running/long_running.cpp
+++ tests/long_running/long_running.cpp
@@ -62,7 +62,7 @@ int main(int argc, char **argv)
 */
 for (int i = 0; i < 200*1000; i++) 
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        //std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
 
         auto *text = const_cast<char *>(u8R"(
