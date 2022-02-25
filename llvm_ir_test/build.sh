afcc6ff42f3e30d24f639d863e311b7c48361f38
nagano1 <nagano1@users.noreply.github.com>
2022-02-26 06:52:30 +0900

    test clang11

--- llvm_ir_test/build.sh
+++ llvm_ir_test/build.sh
@@ -5,14 +5,14 @@ set -e
 
 #clang-10 -S -emit-llvm main.cpp 
 #clang-10 -S -emit-llvm lib.cpp
-COMPILER=clang++-10
-LINKER=clang++-10
+COMPILER=clang++-11
+LINKER=clang++-11
 
 $COMPILER -S -emit-llvm -O3 main.cpp
 $COMPILER -S -emit-llvm -O3 lib.cpp
 
-llc-10 main.ll
-llc-10 lib.ll
+llc-11 main.ll
+llc-11 lib.ll
 
 #-nostdlib
 $LINKER -lpthread  main.s lib.s -o hello && echo start && ./hello
