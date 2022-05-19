382beb0305e3f6970720eaf4fba77c0566ed52ee
nagano1 <nagano1@users.noreply.github.com>
2022-05-20 01:00:39 +0900

    how

--- src/codenodes/code_nodes.cpp
+++ src/codenodes/code_nodes.cpp
@@ -27,9 +27,9 @@ namespace smart {
         ErrorInfo* rhs = (ErrorInfo*)arhs;
 
         if (lhs->errorCode == rhs->errorCode) {
-            //printf("duplicate error id\n");
+            throw 3;
+
             return 0;
-            // throw 3;
         }
         else if (lhs->errorCode > rhs->errorCode) {
             return 1;
