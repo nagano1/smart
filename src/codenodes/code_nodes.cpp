89076197f167436790de8b888ef449d36cbf6224
nagano1 <nagano1@users.noreply.github.com>
2022-05-20 08:00:32 +0900

    a

--- src/codenodes/code_nodes.cpp
+++ src/codenodes/code_nodes.cpp
@@ -20,31 +20,31 @@
 namespace smart {
 
     ErrorInfo ErrorInfoList[errorListSize];
-    static ErrorInfo sortErrorInfoList[errorListSize];
+    //static ErrorInfo sortErrorInfoList[errorListSize];
     bool errorInfoInitialized{false};
 
 
 
-    static int acompare(void const * alhs, void const * arhs) {
-        ErrorInfo* lhs = (ErrorInfo*)alhs;
-        ErrorInfo* rhs = (ErrorInfo*)arhs;
+    //static int acompare(void const * alhs, void const * arhs) {
+    //    ErrorInfo* lhs = (ErrorInfo*)alhs;
+    //    ErrorInfo* rhs = (ErrorInfo*)arhs;
 
-        if (lhs->errorCode == rhs->errorCode) {
-            // printf("duplicate error id(%d)\n ", lhs->errorCode);
-            return 0;
-            // throw 3;
-        }
-        else if (lhs->errorCode > rhs->errorCode) {
-            return 1;
-        }
-        else {
-            return -1;
-        }
+    //    if (lhs->errorCode == rhs->errorCode) {
+    //        // printf("duplicate error id(%d)\n ", lhs->errorCode);
+    //        return 0;
+    //        // throw 3;
+    //    }
+    //    else if (lhs->errorCode > rhs->errorCode) {
+    //        return 1;
+    //    }
+    //    else {
+    //        return -1;
+    //    }
 
-        //return lhs->errorCode - rhs->errorCode;
-        return 0;
+    //    //return lhs->errorCode - rhs->errorCode;
+    //    return 0;
 
-    }
+    //}
 
 
 
