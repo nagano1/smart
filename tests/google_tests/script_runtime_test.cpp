1c84e00a315932dc7c91e31e7fd0e7bcb7940cf7
nagano1 <nagano1@users.noreply.github.com>
2022-08-11 14:17:21 +0900

    fix it

--- tests/google_tests/script_runtime_test.cpp
+++ tests/google_tests/script_runtime_test.cpp
@@ -12,52 +12,52 @@
 #include "../test_common.h"
 #include "script_runtime/script_runtime.hpp"
 
-using namespace smart;
+namespace smart {
+
+
+    /*
+    static void testJson(const char* codeText);
+
+
+    static void testJson(const char* codeText) {
+        auto *document = Alloc::newDocument(DocumentType::JsonDocument, nullptr);
+        DocumentUtils::parseText(document, codeText, strlen(codeText));
+
+        char *treeText = DocumentUtils::getTextFromTree(document);
+        DocumentUtils::generateHashTables(document);
+        EXPECT_EQ(std::string{ treeText }, std::string{ codeText });
+
+        Alloc::deleteDocument(document);
+    }
+    */
 
 
 
-/*
-static void testJson(const char* codeText);
 
 
-static void testJson(const char* codeText) {
-    auto *document = Alloc::newDocument(DocumentType::JsonDocument, nullptr);
-    DocumentUtils::parseText(document, codeText, strlen(codeText));
+    TEST(ScriptTest_, scriptDemo) {
 
-    char *treeText = DocumentUtils::getTextFromTree(document);
-    DocumentUtils::generateHashTables(document);
-    EXPECT_EQ(std::string{ treeText }, std::string{ codeText });
-
-    Alloc::deleteDocument(document);
-}
-*/
-
- 
-
-
-
-TEST(ScriptTest_, scriptDemo) {
-
-    constexpr char text[] = R"(
+        constexpr char text[] = R"(
 fn main() {
     let a = 0
     int b = 0
     print("test日本語")
 }
 )";
-    const char* chars = text;// .c_str();
-    auto* document = Alloc::newDocument(DocumentType::CodeDocument, nullptr);
-    DocumentUtils::parseText(document, chars, sizeof(text) - 1);
-    DocumentUtils::generateHashTables(document);
+        const char* chars = text;// .c_str();
+        auto* document = Alloc::newDocument(DocumentType::CodeDocument, nullptr);
+        DocumentUtils::parseText(document, chars, sizeof(text) - 1);
+        DocumentUtils::generateHashTables(document);
 
-    char* treeText = DocumentUtils::getTextFromTree(document);
-    printf("%s\n", treeText);
+        char* treeText = DocumentUtils::getTextFromTree(document);
+        printf("%s\n", treeText);
 
-    EXPECT_EQ(strcmp(treeText, text), 0);
+        EXPECT_EQ(strcmp(treeText, text), 0);
 
-    startScript(document);
+        startScript(document);
+    }
+
+    ENDTEST
+
+
 }
-
-ENDTEST
-
-
