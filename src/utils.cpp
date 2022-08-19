037ffb2e416b95695b2ed311cc7ab00c29aa9778
nagano1 <nagano1@users.noreply.github.com>
2022-08-19 17:31:46 +0900

    test2

--- src/utils.cpp
+++ src/utils.cpp
@@ -95,7 +95,7 @@ void VoidHashMap::init(MemBuffer* membuffer) {
     this->entries = (VoidHashNode**)this->memBuffer->newMemArray<VoidHashNode>(HashNode_TABLE_SIZE);
     this->entries_length = HashNode_TABLE_SIZE;
 
-    memset((void*)this->entries, 0, sizeof(VoidHashNode*)*this->entries_length);
+    memset((char*)this->entries, 0, sizeof(VoidHashNode*)*this->entries_length);
     for (unsigned int i = 0; i < this->entries_length; i++) {
         this->entries[i] = nullptr;
     }
