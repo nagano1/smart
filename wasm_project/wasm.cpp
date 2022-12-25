8eec0a55002343ae7138e23364416ae7e6526a43
nagano1 <nagano1@users.noreply.github.com>
2022-12-25 22:11:13 +0900

    try

--- wasm_project/wasm.cpp
+++ wasm_project/wasm.cpp
@@ -169,7 +169,7 @@ class kaw{
         //int_vec.push_back(2);
         // print_num((uintptr_t)&int_vec);
 
-        print("{\"isTestResult\":true, \"ok\": false}");
+        print("{\"isTestResult\":true, \"ok\": true}");
 
         if (34 == 34)
         {
